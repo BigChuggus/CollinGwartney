7c700d5d1858dad936f4a65ec088ae368f616cce
Big Chuggus <57680372+BigChuggus@users.noreply.github.com>
2020-04-09 11:23:31 -0500

    Add files via upload

--- Exercises/GuessNumberApp/src/guessNumber/GuessNumberApp.java
+++ Exercises/GuessNumberApp/src/guessNumber/GuessNumberApp.java
@@ -1,6 +1,7 @@
 package guessNumber;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GuessNumberApp {
@@ -11,30 +12,54 @@ public class GuessNumberApp {
         System.out.println("Guess the number!");
         System.out.println("I'm thinking of a number from 1 to " + LIMIT);
         System.out.println();
-        Random random = new Random();
+
         // get a random number between 1 and the limit
         double d = Math.random() * LIMIT; // d is >= 0.0 and < limit
-        int randomNum = (int) d;             // convert double to int
-        int right_guess=randomNum.nextInt(10);
-        randomNum++;                         // int is >= 1 and <= limit
+        int number = (int) d;             // convert double to int
+        number++;                        // int is >= 1 and <= limit
 		
         // prepare to read input from the user
 		Scanner sc = new Scanner(System.in);
+		int count = 1;
 		
-        int guess = sc.nextInt();
-        System.out.println("You guessed: " + guess);
+	   
+		
+		while (true) {
+			try {
+			    int guess = sc.nextInt();
+			 catch(InputMismatchException e) {
+			    System.out.println("some nice error message");
+			    continue;
+			
+	        System.out.println("You guessed: " + guess);
+	        
+	        
+	        if (guess < 1 || guess > LIMIT) {
+	        	System.out.println("Your Guess is Invalid.");
+	        	continue;
+	        }
+	        
+	        if (guess < number) {
+	        	System.out.println("Too Low.");
+	        } else if (guess > number) {
+	        	System.out.println("Too High.");
+	        } else {
+	        	System.out.println("You guessed it in " + count + " tries.\n");
+	        	
+	        	
+	        	 
+	        	  break;
+	        }
+	        
+	        count++;
+		}
         
-        //TODO: compare the guess to the random number
-        
-        
-        //TODO: what should happen if the user enters a number outside the range?
-        if RandomNumber = (< 10) {
-        do	System.out.println("Too Far!")
-        }
-        //TODO: print out a message to the user saying "Too high" or "Too low"
-        
-        
-        
+	        
+        System.out.println("Bye!");
+  
+		}
 	}
+	
 
 }
+
---
+++ Exercises/ch09_ex1_NameParser/src/murach/name/NameParserApp.java
@@ -0,0 +1,38 @@
+package murach.name;
+
+import java.util.Scanner;
+
+public class NameParserApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a name: ");
+        String name = sc.nextLine();
+        
+        
+        name = name.trim();
+        
+        int startSpace = name.indexOf(" ");
+        int endSpace = name.lastIndexOf(" ");
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
+       
+        if (startSpace >= 0)
+        {
+           firstName = name.substring(0, startSpace);
+           if (endSpace > startSpace)
+               middleName = name.substring(startSpace + 1, endSpace);
+               lastName = name.substring(endSpace + 1, name.length());
+
+        }
+        
+        System.out.println("First Name: " + firstName);
+        System.out.println("Middle Name: " + middleName);
+        System.out.println("Last Name: " + lastName);
+        
+        sc.close();
+        
+    }
+}
