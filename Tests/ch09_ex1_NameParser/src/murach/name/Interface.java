dec47109dea6ea248689a5a11f169d6c5394acc1
Big Chuggus <57680372+BigChuggus@users.noreply.github.com>
2020-04-30 21:33:39 -0500

    Name Parser V2 It actually works

--- Tests/ch09_ex1_NameParser/src/murach/name/Interface.java
+++ Tests/ch09_ex1_NameParser/src/murach/name/Interface.java
@@ -3,6 +3,7 @@ package murach.name;
 import murach.name.Interface;
 import javax.swing.JFrame;
 
+import java.awt.Component;
 import java.awt.event.WindowEvent;
 import java.util.Scanner;
 
@@ -13,22 +14,27 @@ public class Interface extends NameParserApp {
 
 		Interface(){  
 		    f=new JFrame();   
-		    String name=JOptionPane.showInputDialog(f,"Enter Full Name");      
+		    String name=JOptionPane.showInputDialog(f,"Enter Full Name"); 
+		    // System.out.println(name);
+		    windowClosing(name);
 		}  
 		public static void main(String[] args) {  
 		    new Interface();  
+		    
+		    
+		    
 		}  
 		  
-		public void windowClosing(WindowEvent e) {  
-		    int a=JOptionPane.showConfirmDialog(f,"Are you sure?");  
-		if(a==JOptionPane.YES_OPTION){  
-		    Scanner sc = new Scanner(System.in);
+		public void windowClosing(String InputName) {  
+		  //  int a=JOptionPane.showConfirmDialog(f,"Are you sure?");  
+		// if(a==JOptionPane.YES_OPTION) {  
+		//    Scanner sc = new Scanner(System.in);
 
-	        System.out.print("Enter your full name: ");
-	        String name = sc.nextLine();
+	   //     System.out.print("Enter your full name: ");
+	   //     String name = sc.nextLine();
 	        
 	        
-	        name = name.trim();
+	        String name = InputName.trim();
 	        
 	        int startSpace = name.indexOf(" ");
 	        int endSpace = name.lastIndexOf(" ");
@@ -44,11 +50,18 @@ public class Interface extends NameParserApp {
 	               lastName = name.substring(endSpace + 1, name.length());
 
 	        }
-	        
-	        System.out.println("First Name: " + firstName);
-	        System.out.println("Middle Name: " + middleName);
-	        System.out.println("Last Name: " + lastName);
-		}  
+	     //   Component g;
+	     //   g = new Component();
+	       JOptionPane.showMessageDialog(null, 
+	        		"First Name: " + firstName + "\n" + 
+	        		"Middle Name: " + middleName + "\n" + 
+	        		"Last Name: " + lastName, "Name Parser", JOptionPane.PLAIN_MESSAGE);
+	        		// JOptionPane.OK_OPTION, JOptionPane.WARNING_MESSAGE
+	        		
+	      //  System.out.println("First Name: " + firstName);
+	      //  System.out.println("Middle Name: " + middleName);
+	      //  System.out.println("Last Name: " + lastName);
+		// }  
 		}
 
 }
